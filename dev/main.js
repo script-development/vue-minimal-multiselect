1d59d1ed6a6b829a8acd454efe4b7223e276e13e
Gerard Oosterhof <gerard.oosterhof@gmail.com>
2020-09-21 15:08:36 +0200

    Big refactor

--- dev/main.js
+++ dev/main.js
@@ -1,3 +1,27 @@
 import Vue from 'vue';
+import MinimalMultiselect from '../dist/index.esm';
+import Multiselect from 'vue-multiselect';
 
-new Vue({render: h => h('div', 'moi')}).$mount('#app');
+const options = [
+    {id: 1, name: 'python'},
+    {id: 2, name: 'php'},
+];
+
+new Vue({
+    data: {
+        minimalSelected: [],
+        selected: [],
+    },
+    render(h) {
+        const minimalMulti = h(MinimalMultiselect, {
+            props: {options, value: this.minimalSelected, placeholder: 'pick an option'},
+            on: {input: options => (this.minimalSelected = options)},
+        });
+
+        const multi = h(Multiselect, {
+            props: {options, value: this.selected, trackBy: 'id', label: 'name', multiple: true},
+            on: {input: options => (this.selected = options)},
+        });
+        return h('div', [minimalMulti, multi]);
+    },
+}).$mount('#app');
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import multiselectoptions from './multiselectoptions';
-import multiselect_selectedoptions from './multiselectselectedoptions';
-import multiselect_searchfield from './multiselectsearchfield';
+/**
+ * @typedef {import('vue').CreateElement} CreateElement
+ */
 
 export default {
     name: 'minimal-multiselect',
@@ -10,11 +10,7 @@ export default {
             required: false,
             default: 'id',
         },
-        customtext: {
-            type: String,
-            required: false,
-        },
-        label: {
+        textField: {
             type: String,
             required: false,
             default: 'name',
@@ -27,7 +23,7 @@ export default {
             type: String,
             required: false,
         },
-        selected: {
+        value: {
             type: Array,
             required: true,
         },
@@ -45,71 +41,180 @@ export default {
     },
     computed: {
         filteredOptions() {
+            const selectedAmount = this.value.length;
+
+            if (!this.findOption && !selectedAmount) return this.options;
+
+            const search = this.findOption.toLowerCase();
+
             return this.options.filter(option => {
-                if (!option[this.label]) option = {[this.label]: option};
-                return (
-                    option[this.label].toLowerCase().includes(this.findOption.toLowerCase()) &&
-                    !this.selected.some(selectedOption => selectedOption[this.valueField] == option[this.valueField])
-                );
+                if (search && !option[this.textField].toLowerCase().includes(search)) return false;
+                if (selectedAmount && this.value.indexOf(option[this.valueField]) !== -1) return false;
+                return true;
             });
         },
     },
     methods: {
+        pickOption(option) {
+            this.$emit('input', [...this.value, option[this.valueField]]);
+            this.clearDropdown();
+        },
         addOption() {
-            if (this.selected.includes(this.findOption)) {
-                return;
-            }
-            if (!this.findOption.length) {
-                return;
-            }
-            this.$emit('create', this.findOption);
+            if (this.value.findIndex(option => option[this.textField] === this.findOption) !== -1) return;
+
+            if (this.$listeners.create) return this.$emit('create', this.findOption);
+
+            this.$emit('input', [...this.value, {[this.textField]: this.findOption}]);
+            this.clearDropdown();
+        },
+        removeOption(optionValue) {
+            this.$emit(
+                'input',
+                this.value.filter(value => value !== optionValue)
+            );
+            this.clearDropdown();
+        },
+        clearDropdown() {
+            this.dropDownEnabled = false;
             this.findOption = '';
-            document.getElementById('searchField').value = this.findOption;
         },
-        selectOption(option) {
-            if (this.selected.includes(option)) {
-                return;
-            }
-            this.findOption = '';
-            document.getElementById('searchField').value = this.findOption;
-            this.options.splice(this.options.indexOf(option));
-            this.$emit('tag', [
-                ...this.selected.map(option => option[this.valueField] || option),
-                option[this.valueField],
-            ]);
-        },
-        removeOption(option) {
-            if (!this.options.includes(option)) {
-                this.selected.splice(this.selected.indexOf(option), 1);
-                return;
-            }
-            this.filteredOptions.push(option);
-            this.selected.splice(this.selected.indexOf(option), 1);
-        },
-        filterOption(event) {
-            this.findOption = event.target.value;
-        },
-        toggleDropdown() {
-            this.dropDownEnabled = !this.dropDownEnabled;
+        inputEnter() {
+            const filteredOptionsLength = this.filteredOptions.length;
+
+            if (!filteredOptionsLength) return this.addOption();
+            if (filteredOptionsLength === 1) return this.pickOption(this.filteredOptions[0]);
         },
     },
 
+    /** @param {CreateElement} h */
     render(h) {
-        let selected = this.selected;
-        let options = this.filteredOptions;
-        return h(
+        const select = h('div', {
+            class: 'multiselect__select',
+            on: {
+                click: () => {
+                    if (this.dropDownEnabled) return this.clearDropdown();
+                    this.dropDownEnabled = true;
+                },
+            },
+        });
+
+        const tagsWrap = h('div', {class: 'multiselect__tags-wrap'}, [
+            this.value.map(optionValue => {
+                const option = this.options.find(option => option[this.valueField] === optionValue);
+                const optionText = option ? option[this.textField] : optionValue[this.textField];
+
+                // using optionText as key, big assumption that those are unique
+                return h('span', {class: 'multiselect__tag', key: optionText}, [
+                    h('span', [optionText]),
+                    h('i', {
+                        class: 'multiselect__tag-icon',
+                        attrs: {'aria-hidden': 'true'},
+                        on: {
+                            mousedown: () => this.removeOption(optionValue),
+                        },
+                    }),
+                ]);
+            }),
+        ]);
+
+        const searchField = h('input', {
+            class: 'multiselect__input',
+            attrs: {
+                placeholder: this.placeholder,
+                value: this.findOption,
+                style: this.dropDownEnabled ? 'width:100%;' : 'width: 0px; position:absolute; padding:0px;',
+            },
+            on: {
+                input: event => (this.findOption = event.target.value),
+                keypress: event => {
+                    if (event.charCode == 13) {
+                        // charCode 13 is enter key
+                        event.preventDefault();
+                        this.inputEnter();
+                    }
+                },
+            },
+        });
+
+        if (this.dropDownEnabled) {
+            this.$nextTick(() => {
+                // TODO :: find a way to make this work gracefully
+                if (!this.findOption) searchField.elm.value = '';
+                // TODO :: find a way to focus only on opening
+                searchField.elm.focus();
+            });
+        }
+
+        const tagChildren = [tagsWrap, searchField];
+
+        if (!this.dropDownEnabled && !this.value.length) {
+            tagChildren.push(h('span', {class: 'multiselect__placeholder'}, [this.placeholder]));
+        }
+
+        const tags = h(
             'div',
             {
-                attrs: {class: 'multiselect', tabindex: '-1'},
-                on: {blur: this.toggleDropdown, click: () => this.toggleDropdown},
+                class: 'multiselect__tags',
+                on: {
+                    click: () => (this.dropDownEnabled = true),
+                    focusout: () => {
+                        // TODO :: it's not really pretty, but it works
+                        setTimeout(this.clearDropdown, 200);
+                    },
+                },
             },
-            [
-                h(multiselect_selectedoptions, {
-                    props: {selected: selected, options: options},
-                }),
-                h(multiselect_searchfield, {props: {placeholder: this.placeholder}}),
-                h(multiselectoptions, {props: {options: options}}),
-            ]
+            tagChildren
         );
+
+        const optionItems = this.filteredOptions.map(option =>
+            h(
+                'li',
+                {
+                    class: 'multiselect__element',
+                    key: option[this.textField],
+                    attrs: {style: 'cursor: pointer;'},
+                    on: {click: () => this.pickOption(option)},
+                },
+                [h('span', {class: 'multiselect__option'}, [option[this.textField]])]
+            )
+        );
+        optionItems.push(
+            h(
+                'li',
+                {
+                    attrs: {
+                        style: this.filteredOptions.length || !this.findOption ? 'display:none;' : 'cursor: pointer;',
+                    },
+                    on: {click: () => this.addOption()},
+                },
+                [h('span', {class: 'multiselect__option'}, [this.noResults])]
+            )
+        );
+
+        const options = h('ul', {class: 'multiselect__content', attrs: {style: 'display:inline-block'}}, optionItems);
+
+        const optionsWrapper = h(
+            'div',
+            {
+                class: 'multiselect__content-wrapper',
+                attrs: {
+                    tabindex: '-1',
+                    style: this.dropDownEnabled ? 'max-height: 300px;' : 'max-height: 300px; display:none;',
+                },
+            },
+            [options]
+        );
+
+        const children = [select, tags, optionsWrapper];
+
+        const minimalMultiselect = h(
+            'div',
+            {
+                class: this.dropDownEnabled ? 'multiselect multiselect--active' : 'multiselect',
+            },
+            children
+        );
+
+        return minimalMultiselect;
     },
 };
